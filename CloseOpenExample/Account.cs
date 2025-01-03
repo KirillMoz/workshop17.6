990c2842d68a74dba7ce1e74acd7263d781c9031
KirillMoz <kirill.mozzhegorov@yandex.ru>
2025-01-04 03:04:19 +0500

    Промежуточное закрытие

---
+++ CloseOpenExample/Account.cs
@@ -0,0 +1,26 @@
+﻿using CloseOpenExample.TypesInterestRateCalculation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloseOpenExample
+{
+    public class Account
+    {
+        // тип учетной записи
+        public string Type { get; set; }
+
+        // баланс учетной записи
+        public double Balance { get; set; }
+
+        // процентная ставка
+        public double Interest { get; set; }
+
+        public double CalcInterest(ICalculationInterest CalculationInterest)
+        {
+            return CalculationInterest.ExecuteCalc(this.Balance);
+        }
+    }
+}
